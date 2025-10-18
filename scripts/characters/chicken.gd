0e43da7c80bbb3a438ec09933af7083f4f1a93b5
night-shooter <artem.math.bg@gmail.com>
2025-10-18 13:14:50 +0500

    adding cow npc

--- scripts/characters/chicken.gd
+++ scripts/characters/chicken.gd
@@ -1,4 +1,1 @@
 extends NonPlayableCharacter
-
-func _ready() -> void:
-    walk_cycles = randi_range(min_walk_cycle, max_walk_cycle)
--- scripts/characters/non_playable_character.gd
+++ scripts/characters/non_playable_character.gd
@@ -6,3 +6,6 @@ extends CharacterBody2D
 
 var walk_cycles: int
 var current_walk_cycle: int
+
+func _ready() -> void:
+    walk_cycles = randi_range(min_walk_cycle, max_walk_cycle)
