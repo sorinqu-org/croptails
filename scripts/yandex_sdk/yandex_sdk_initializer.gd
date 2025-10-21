d66992e68a0bc510ac24b3861b7658ea0b50bb5c
night-shooter <artem.math.bg@gmail.com>
2025-10-21 09:08:45 +0500

    SMALL CHAGES: adding godotgif plugin and test web build

--- scripts/yandex_sdk/yandex_sdk_initializer.gd
+++ scripts/yandex_sdk/yandex_sdk_initializer.gd
@@ -3,6 +3,12 @@ extends Node
 
 @export var menu_scene: PackedScene
 
-func _ready() -> void:
-	await YandexSDK.game_ready()
-	get_tree().change_scene_to_packed(menu_scene)
+func _ready() -> void:	
+
+	await YandexSDK.game_ready()	
+	print("Yandex SDK successfully initialized!")
+	
+	await get_tree().create_timer(1).timeout
+	get_tree().call_deferred("change_scene_to_packed", menu_scene)
+	YandexSDK.gameplay_started()
+	queue_free()
